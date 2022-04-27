8178336719213c19387db34b21d0b6a90cc93563
Rabi Gautam <rabi@sharelookapp.com>
2022-04-27 21:52:04 +0545

    Revert "Revert "package added""
    This reverts commit 07afd2b1223ca8c115a0d020121d8f7f657b65a7.

---
+++ pkg/models/book.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"github.com/jinzhu/gorm"
+	"github.com/rabigautam/book_management_system/pkg/config"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+func (b *Book) CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
