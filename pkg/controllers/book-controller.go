5335f93e052f9e6ecbc6f952bfebe985c5fa68d8
Rabi Gautam <rabi@sharelookapp.com>
2022-04-28 10:09:36 +0545

    feature:impl update operation

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -31,7 +31,7 @@ func DeleteBookById(w http.ResponseWriter, r *http.Request) {
 	}
 	book := models.DeleteBookById(ID)
 	res, _ := json.Marshal(book)
-	w.Header().Set("Content-Type","application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 
@@ -40,22 +40,50 @@ func DeleteBookById(w http.ResponseWriter, r *http.Request) {
 func GetBookById(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	bookId := params["id"]
-	id,err := strconv.ParseInt(bookId,0,0)
-	if err!=nil{
+	id, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
 		fmt.Println(err)
 	}
 	bookDetails, _ := models.GetBookById(id)
-	book,_:=json.Marshal(bookDetails)
-	w.Header().Set("Content-Type","application/json")
+	book, _ := json.Marshal(bookDetails)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 
 	w.Write(book)
 }
 
-func GetBooks(w http.ResponseWriter, r *http.Request){
-	books:=models.GetBooks()
-	res,_:=json.Marshal(books)
-	w.Header().Set("Content-Type","application/json")
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	books := models.GetBooks()
+	res, _ := json.Marshal(books)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var updateBook = &models.Book{}
+	utils.ParseBody(r, updateBook)
+	params := mux.Vars(r)
+	id := params["id"]
+	bookId, err := strconv.ParseInt(id, 0, 0)
+	if err != nil {
+		fmt.Println(err)
+
+	}
+
+	getBook, db := models.GetBookById(bookId)
+	if updateBook.Name != "" {
+		getBook.Name = updateBook.Name
+	}
+	if updateBook.Author != "" {
+		getBook.Author = updateBook.Author
+	}
+	if updateBook.Publication != "" {
+		getBook.Publication = updateBook.Publication
+	}
+	db.Save(&getBook)
+	res, _ := json.Marshal(getBook)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
--- pkg/routes/books-route.go
+++ pkg/routes/books-route.go
@@ -8,8 +8,9 @@ import(
 
 var RegisterBookStoreRoutes = func(router *mux.Router){
 	router.HandleFunc("/book/",controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/{id}/",controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/book/",controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{id}",controllers.DeleteBookById).Methods("DELETE")
-	router.HandleFunc("/book/{id}",controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{id}/",controllers.DeleteBookById).Methods("DELETE")
+	router.HandleFunc("/book/{id}/",controllers.GetBookById).Methods("GET")
 
 }
